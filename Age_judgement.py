ec93e9df7a11e7dc76f941a3708c8c81962b9941
CHYUN <chenfwulei@gmail.com>
2017-11-01 20:54:44 +0800

    Create Age_judgement.py

---
+++ Age_judgement.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/5/18 21:26
+# @Author  : chenf
+# @Site    : 
+# @File    : Age_judgement.py
+# @Software: PyCharm
+
+age = 20
+if age >= 6:
+    print('teenager')
+elif age >= 18:
+    print('adult')
+else:
+    print('kid')
